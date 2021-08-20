1842cdede0c44dcd1dd574f9008e55af2ad81283
gianluCava <86367223+gianluCava@users.noreply.github.com>
2021-08-19 22:15:47 -0300

    Update and rename EX10(sorted-union).txt to sorted-union.js

--- sorted-union.js
+++ sorted-union.js
@@ -15,26 +15,7 @@ function uniteUnique(...arr) {
       if(newarr.indexOf(arr[i][j]) === -1) {newarr.push(arr[i][j])}
     }
   }
-  
-  return newarr
+   return newarr
 }
 
 console.log(uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]));
-/*
-OTRAS SOLUCIONES
-function uniteUnique(...arr) {
-  return [...new Set(arr.flat())]
-}
-
-// Or as an arrow function
-const uniteUnique = (...arr) => [...new Set(arr.flat())]
-------------------------------------------------------------
-
-function uniteUnique(...arrays) {
-  //make an array out of the given arrays and flatten it (using the spread operator)
-  const flatArray = [].concat(...arrays);
-
-  // create a Set which clears any duplicates since it's a regular set and not a multiset
-  return [...new Set(flatArray)];
-}
- */
